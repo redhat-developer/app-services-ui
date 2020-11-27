f05d7602f483a91b4628e353184cc4b50c647753
Pete Muir <pmuir@bleepbleep.org.uk>
2020-11-27 19:31:58 +0000

    feat: route alerts from control plane ui to cloud.redhat.com notifications

--- src/app/ControlPlanePage/ControlPlanePage.tsx
+++ src/app/ControlPlanePage/ControlPlanePage.tsx
@@ -1,30 +1,45 @@
 // @ts-ignore
-import { OpenshiftStreamsFederated } from 'mkUiFrontend/OpenshiftStreams';
+import {OpenshiftStreamsFederated} from 'mkUiFrontend/OpenshiftStreams';
 import React, {useContext, useEffect, useState} from 'react';
-import { useHistory } from 'react-router';
+import {useHistory} from 'react-router';
 import {InsightsContext} from "@app/utils/insights";
+import {useDispatch} from 'react-redux';
+import {addNotification} from '@redhat-cloud-services/frontend-components-notifications/';
+import {AlertVariant} from "@patternfly/react-core";
 
 export const ControlPlanePage = () => {
 
-    const [token, setToken] = useState('');
+  const [token, setToken] = useState('');
 
-    const insights = useContext(InsightsContext);
+  const insights = useContext(InsightsContext);
 
-    useEffect(() => {
-        insights.chrome.auth.getToken().then(t => setToken(t));
-    }, []);
+  useEffect(() => {
+    insights.chrome.auth.getToken().then(t => setToken(t));
+  }, []);
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const onConnectInstance = async (event) => {
-        if (event.id === undefined) {
-            throw new Error();
-        }
+  const onConnectInstance = async (event) => {
+    if (event.id === undefined) {
+      throw new Error();
+    }
 
-        history.push(`/kafkas/${event.id}`);
-    };
+    history.push(`/kafkas/${event.id}`);
+  };
 
-    return (
-        <OpenshiftStreamsFederated token={token} onConnectToInstance={onConnectInstance}/>
+  const dispatch = useDispatch();
+
+  const addAlert = (message: string, variant?: AlertVariant) => {
+    dispatch(
+      addNotification({
+        variant: variant,
+        title: message
+      })
     );
+
+  };
+
+  return (
+    <OpenshiftStreamsFederated token={token} onConnectToInstance={onConnectInstance} addAlert={addAlert}/>
+  );
 };
