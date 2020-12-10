860d2e5a04ab2f0c2d3a024ae0cc145e0d10a2b4
Pete Muir <pmuir@bleepbleep.org.uk>
2020-12-10 01:47:48 +0000

    feat: switch to topic list

--- src/app/DataPlanePage/DataPlanePage.tsx
+++ src/app/DataPlanePage/DataPlanePage.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {FunctionComponent, useContext} from 'react';
 import {FederatedModule} from "../../Components/FederatedModule";
+import {InsightsContext} from "@app/utils";
+import {RouteComponentProps, useLocation, useParams} from "react-router-dom";
 
-export const DataPlanePage = () => {
+type DataPlanePageParams = {
+  id: string
+}
+
+export const DataPlanePage = ( {match}: RouteComponentProps<DataPlanePageParams>) => {
+
+  const insights = useContext(InsightsContext);
+
+  // TODO useParams is not working?
+  const pathname = window.location.pathname.endsWith("/") ? window.location.pathname : `${window.location.pathname}/`;
+  const parts = pathname.split("/");
+  const id = parts[parts.length - 2];
+
+  console.log(parts);
+
+  console.log(id);
+
+  const getDataPlaneToken = () => {
+    return "redhat123";
+  }
+
+
   return (
     <FederatedModule
       scope="strimziUi"
-      module="./Panels/Home.patternfly"
-      render={(Home) => <Home/>}
+      module="./Panels/Topics.patternfly"
+      render={(FederatedTopics) => <FederatedTopics
+        getControlPlaneToken={insights.chrome.auth.getToken}
+        getDataPlaneToken={getDataPlaneToken}
+        id={id}
+      />}
     />
   );
 }
