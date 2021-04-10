67359d91ffa0937f6c4c1165dab6a7c948058653
ajaypratap003 <ajay.pratap233@gmail.com>
2021-04-11 00:23:16 +0530

    add connect to serviec accounts path

--- src/app/KasPage/KasPage.tsx
+++ src/app/KasPage/KasPage.tsx
@@ -1,18 +1,17 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router';
-import { InsightsContext } from "@app/utils/insights";
+import { InsightsContext } from '@app/utils/insights';
 import { useDispatch } from 'react-redux';
 import { addNotification } from '@redhat-cloud-services/frontend-components-notifications/';
-import { AlertVariant } from "@patternfly/react-core";
-import { FederatedModule } from "../Components/FederatedModule/FederatedModule";
-import { ConfigContext } from "@app/Config/Config";
-import { Loading } from "@app/Components/Loading/Loading";
-import { Configuration, DefaultApi,TermsReviewResponse } from "../../openapi/ams";
-import { getTermsAppURL } from "@app/utils/termsApp";
+import { AlertVariant } from '@patternfly/react-core';
+import { FederatedModule } from '../Components/FederatedModule/FederatedModule';
+import { ConfigContext } from '@app/Config/Config';
+import { Loading } from '@app/Components/Loading/Loading';
+import { Configuration, DefaultApi, TermsReviewResponse } from '../../openapi/ams';
+import { getTermsAppURL } from '@app/utils/termsApp';
 import queryString from 'query-string';
 
 export const KasPage: React.FunctionComponent = () => {
-
   const insights = useContext(InsightsContext);
   const config = useContext(ConfigContext);
 
@@ -37,17 +36,17 @@ export const KasPage: React.FunctionComponent = () => {
 
   useEffect(() => {
     // Load the terms review state asynchronously, to avoid the user waiting when they press the Create Kafka Instance button
-    const selfTermsReview = async() => {
+    const selfTermsReview = async () => {
       const accessToken = await insights.chrome.auth.getToken();
       const ams = new DefaultApi({
         accessToken,
         basePath: config?.controlPlane.amsBasePath || '',
       } as Configuration);
-      setTermsReview(await ams.apiAuthorizationsV1SelfTermsReviewPost().then(resp => resp.data));
-    }
+      setTermsReview(await ams.apiAuthorizationsV1SelfTermsReviewPost().then((resp) => resp.data));
+    };
 
     selfTermsReview();
-  },[config?.controlPlane.amsBasePath, insights.chrome.auth]);
+  }, [config?.controlPlane.amsBasePath, insights.chrome.auth]);
 
   const onConnectInstance = async (event) => {
     if (event.id === undefined) {
@@ -61,14 +60,22 @@ export const KasPage: React.FunctionComponent = () => {
       throw new Error();
     }
     return history.createHref({ pathname: `/streams/kafkas/${event.id}` });
-  }
+  };
+
+  const onConnectToServiceAccounts = async (event) => {
+    history.push('/streams/service-accounts');
+  };
+
+  const getConnectToServiceAcountsPath = (event) => {
+    return history.createHref({ pathname: '/streams/service-accounts' });
+  };
 
   const preCreateInstance = async (open: boolean) => {
     // if termsReview is set, we can proceed, otherwise wait for the effect to complete - the state update will cause the page to rerender
     if (termsReview) {
       if (termsReview.terms_available || termsReview.terms_required) {
         if (termsReview.redirect_url === undefined) {
-          throw new Error("terms must be signed but there is no terms url");
+          throw new Error('terms must be signed but there is no terms url');
         }
         const redirectURL = queryString.stringifyUrl({ url: window.location.href, query: { create: 'true' } });
         const url = getTermsAppURL(termsReview.redirect_url, redirectURL, window.location.href);
@@ -78,12 +85,11 @@ export const KasPage: React.FunctionComponent = () => {
       return open;
     }
     return false;
-
-  }
+  };
 
   const createDialogOpen = () => {
     return create;
-  }
+  };
 
   const dispatch = useDispatch();
 
@@ -91,17 +97,16 @@ export const KasPage: React.FunctionComponent = () => {
     dispatch(
       addNotification({
         variant: variant,
-        title: message
+        title: message,
       })
     );
-
   };
 
   if (config === undefined || termsReview === undefined) {
-    return <Loading/>
+    return <Loading />;
   }
 
-  const getUsername = () => insights.chrome.auth.getUser().then(user => user.identity.user.username);
+  const getUsername = () => insights.chrome.auth.getUser().then((user) => user.identity.user.username);
 
   const osStreams = (
     <FederatedModule
@@ -118,6 +123,8 @@ export const KasPage: React.FunctionComponent = () => {
             createDialogOpen={createDialogOpen}
             addAlert={addAlert}
             basePath={config?.controlPlane.serviceApiBasePath}
+            onConnectToServiceAccounts={onConnectToServiceAccounts}
+            getConnectToServiceAcountsPath={getConnectToServiceAcountsPath}
           />
         );
       }}
