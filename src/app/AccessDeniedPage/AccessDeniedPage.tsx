725cc801704179d7c6dfa661d2aeb5acf4b720f6
Donald Labaj <dlabaj@redhat.com>
2021-04-13 15:21:02 -0400

    Updated with latest design

--- src/app/AccessDeniedPage/AccessDeniedPage.tsx
+++ src/app/AccessDeniedPage/AccessDeniedPage.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 
 import { Main, NotAuthorized } from "@redhat-cloud-services/frontend-components";
  
-import { useTranslation } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
+import { Link } from 'react-router-dom';
+
+interface GetLinkProps {
+   href?: string
+}
+
+const GetLink: React.FunctionComponent<GetLinkProps> = ({href, children}) => <Link to={href || ""}>{children}</Link>
 
 export const AccessDenniedPage: React.FunctionComponent = () => {
   const { t } = useTranslation();
+ 
+ 
+  const accessDeniedDetails = <Trans i18nKey='accessdenied.accessDeniedDescription' t={t} components={[<GetLink href='/streams/kafkas'/>]}/>
 
   return (<Main>
-    <NotAuthorized title={t('accessdenied.accessDeniedTitle')} description={t('accessdenied.accessDeniedDescription')} prevPageButtonText={t('accessdenied.accessDeniedButtonText')} showReturnButton={true}/>
+    <NotAuthorized title={t('accessdenied.accessDeniedTitle')} description={accessDeniedDetails} prevPageButtonText={t('accessdenied.accessDeniedButtonText')} showReturnButton={true}/>
   </Main>
   );
 };
