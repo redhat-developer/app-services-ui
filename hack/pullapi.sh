5fad759d05f1f09d05104bcba5542b74ff3dab83
ajaypratap003 <ajay.pratap233@gmail.com>
2021-05-19 17:39:11 +0530

    update kas openapi yml file path

--- hack/pullapi.sh
+++ hack/pullapi.sh
@@ -1,7 +1,7 @@
 
 echo "Synchronizing managed-services-api & AMS"
 
-curl --insecure -L -o hack/kas-fleet-manager.yaml https://gitlab.cee.redhat.com/service/managed-services-api/-/raw/master/openapi/kas-fleet-manager.yaml
+curl --insecure -L -o hack/kas-fleet-manager.yaml https://gitlab.cee.redhat.com/service/kas-fleet-manager/-/raw/master/openapi/kas-fleet-manager.yaml
 
 curl  -L -o hack/ams.json https://api.stage.openshift.com/api/accounts_mgmt/v1/openapi
 
