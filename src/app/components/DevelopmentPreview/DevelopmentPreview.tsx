51b2ebe0b5a28e94d8e8291d34fadaf52d04556a
ajaypratap003 <ajay.pratap233@gmail.com>
2021-05-27 22:12:14 +0530

    fix lint errors

--- src/app/components/DevelopmentPreview/DevelopmentPreview.tsx
+++ src/app/components/DevelopmentPreview/DevelopmentPreview.tsx
@@ -4,22 +4,25 @@ import { useTranslation } from 'react-i18next';
 
 export const DevelopmentPreview: React.FunctionComponent = ({ children }) => {
   const { t } = useTranslation();
-  return <div id='scrollablePageMain' className='pf-c-page__main' style={{height: '100%'}}>
-    <Banner isSticky variant="info">
-      <Bullseye>  
-        <Popover
-          aria-label="Development Preview Button"
-          hasAutoWidth
-          bodyContent={
-                <div>{t('common.developmentPreviewTooltip')}</div>
-              }
-          position='bottom'
-          minWidth='300px'
-          maxWidth='25%'> 
-            <Button className='app-services-ui--button--dev-preview' variant='link'>{t('common.developmentPreview')}</Button>
+  return (
+    <div id="scrollablePageMain" className="pf-c-page__main" style={{ height: '100%' }}>
+      <Banner isSticky variant="info">
+        <Bullseye>
+          <Popover
+            aria-label="Development Preview Button"
+            hasAutoWidth
+            bodyContent={<div>{t('common.developmentPreviewTooltip')}</div>}
+            position="bottom"
+            minWidth="300px"
+            maxWidth="25%"
+          >
+            <Button className="app-services-ui--button--dev-preview" variant="link">
+              {t('common.developmentPreview')}
+            </Button>
           </Popover>
-      </Bullseye> 
-    </Banner>
-    {children}
-  </div>
-}
+        </Bullseye>
+      </Banner>
+      {children}
+    </div>
+  );
+};
--- src/app/pages/AccessDenied/AccessDeniedPage.tsx
+++ src/app/pages/AccessDenied/AccessDeniedPage.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 
-import { Main, NotAuthorized } from "@redhat-cloud-services/frontend-components";
+import { Main, NotAuthorized } from '@redhat-cloud-services/frontend-components';
 
 import { useTranslation, Trans } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 interface GetLinkProps {
-   href?: string
+  href?: string;
 }
 
-const GetLink: React.FunctionComponent<GetLinkProps> = ({href, children}) => <Link to={href || ""}>{children}</Link>
+const GetLink: React.FunctionComponent<GetLinkProps> = ({ href, children }) => <Link to={href || ''}>{children}</Link>;
 
 export const AccessDeniedPage: React.FunctionComponent = () => {
   const { t } = useTranslation();
 
+  const accessDeniedDetails = (
+    <Trans
+      i18nKey="accessdenied.accessDeniedDescription"
+      t={t}
+      components={[<GetLink href="/streams/kafkas" key="get-link" />]}
+    />
+  );
 
-  const accessDeniedDetails = <Trans i18nKey='accessdenied.accessDeniedDescription' t={t} components={[<GetLink href='/streams/kafkas'/>]}/>
-
-  return (<Main>
-    <NotAuthorized title={t('accessdenied.accessDeniedTitle')} description={accessDeniedDetails} prevPageButtonText={t('accessdenied.accessDeniedButtonText')} showReturnButton={true}/>
-  </Main>
+  return (
+    <Main>
+      <NotAuthorized
+        title={t('accessdenied.accessDeniedTitle')}
+        description={accessDeniedDetails}
+        prevPageButtonText={t('accessdenied.accessDeniedButtonText')}
+        showReturnButton={true}
+      />
+    </Main>
   );
 };
