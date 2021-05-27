116450ce9d988a8d0f7c286113ec7f22c5bceba2
ajaypratap003 <ajay.pratap233@gmail.com>
2021-05-27 22:05:52 +0530

    fix alert issue

--- src/AppEntry.tsx
+++ src/AppEntry.tsx
@@ -5,34 +5,74 @@ import { init } from '@app/store';
 import App from '@app/App';
 import logger from 'redux-logger';
 import getBaseName from '@app/utils/getBaseName';
-import { InsightsType } from "@app/utils/insights";
-import { ConfigProvider } from "@app/providers/ConfigContextProvider";
-import { KeycloakInstance } from "keycloak-js";
-import { Alert, AlertContext, Auth, AuthContext, useConfig } from "@bf2/ui-shared";
-import { getKeycloakInstance, getValidAccessToken } from "@app/utils/keycloakAuth";
+import { InsightsType } from '@app/utils/insights';
+import { ConfigProvider } from '@app/providers/ConfigContextProvider';
+import { KeycloakInstance } from 'keycloak-js';
+import { Alert, AlertContext, Auth, AuthContext, useConfig } from '@bf2/ui-shared';
+import { getKeycloakInstance, getValidAccessToken } from '@app/utils/keycloakAuth';
 import { I18nextProvider } from 'react-i18next';
 import appServicesi18n from '@app/i18n';
-import { AlertVariant } from "@patternfly/react-core";
+import { AlertVariant } from '@patternfly/react-core';
 import { addNotification } from '@redhat-cloud-services/frontend-components-notifications/';
-import { Loading } from "@app/components/Loading/Loading";
+import { Loading } from '@app/components/Loading/Loading';
 
 declare const __webpack_public_path__: string;
+/**
+ * Remove after upgrade @bf2/ui-shared package
+ */
+export type AlertProps = {
+  /**
+   * Flag to automatically call `onDismiss` after `dismissDelay` runs out.
+   */
+  autoDismiss?: boolean;
+  /**
+   * Flag to show/hide notification close button.
+   */
+  dismissable?: boolean;
+  /**
+   * Alert variant
+   */
+  variant: AlertVariant;
+  /**
+   * Alert title
+   */
+  title: string;
+  /**
+   * Alert description
+   */
+  description?: string | React.ReactElement;
+  /**
+   * Time period after which `onDismiss` is called.
+   */
+  dismissDelay?: number;
+  /**
+   * Unique request ID.
+   */
+  requestId?: string;
+  /**
+   * Unique sentry error ID.
+   */
+  sentryId?: string;
+  /**
+   * data-testid attribute
+   */
+  dataTestId?: string;
+};
 
 const AppWithKeycloak: React.FunctionComponent = () => {
-
   const config = useConfig();
 
   React.useEffect(() => {
     if (config != undefined) {
       const loadToken = async () => {
-        const keycloak = await getKeycloakInstance ({
+        const keycloak = await getKeycloakInstance({
           url: config.masSso.authServerUrl,
           clientId: config.masSso.clientId,
-          realm: config.masSso.realm
+          realm: config.masSso.realm,
         });
         setKeycloak(keycloak);
         setLoadingKeycloak(false);
-      }
+      };
       loadToken();
     }
   }, [config]);
@@ -42,65 +82,78 @@ const AppWithKeycloak: React.FunctionComponent = () => {
 
   const dispatch = useDispatch();
 
-
   if (loadingKeycloak || keycloak === undefined) {
-    return <Loading/>;
+    return <Loading />;
   }
 
-  const insights: InsightsType = window["insights"];
+  const insights: InsightsType = window['insights'];
 
   const getToken = () => {
     return getValidAccessToken();
-
-  }
+  };
 
   const auth: Auth = {
-    getUsername: () => insights.chrome.auth.getUser().then(value => value.identity.user.username),
+    getUsername: () => insights.chrome.auth.getUser().then((value) => value.identity.user.username),
     kafka: {
-      getToken
+      getToken,
     },
     kas: {
-      getToken: insights.chrome.auth.getToken
+      getToken: insights.chrome.auth.getToken,
     },
     ams: {
-      getToken: insights.chrome.auth.getToken
-    }
-  }
+      getToken: insights.chrome.auth.getToken,
+    },
+  };
 
-  const addAlert = (message: string, variant?: AlertVariant) => {
+  const addAlert = ({
+    title,
+    variant,
+    description,
+    dataTestId,
+    autoDismiss,
+    dismissable,
+    dismissDelay,
+    requestId,
+    sentryId,
+  }: AlertProps) => {
     dispatch(
       addNotification({
-        variant: variant,
-        title: message
+        title,
+        variant,
+        description,
+        dataTestId,
+        autoDismiss,
+        dismissable,
+        dismissDelay,
+        requestId,
+        sentryId,
       })
     );
   };
 
   const alert: Alert = {
-    addAlert
-  }
+    addAlert,
+  };
 
   const baseName = getBaseName(window.location.pathname);
   return (
     <AuthContext.Provider value={auth}>
       <AlertContext.Provider value={alert}>
         <Router basename={baseName}>
-          <App/>
+          <App />
         </Router>
       </AlertContext.Provider>
     </AuthContext.Provider>
-  )
-}
-
+  );
+};
 
 const AppEntry: React.FunctionComponent = () => (
   <Provider store={init(logger).getStore()}>
-    <I18nextProvider i18n = {appServicesi18n}>
-    <ConfigProvider configUrl={`${__webpack_public_path__}config.json`}>
-      <AppWithKeycloak/>
-    </ConfigProvider>
+    <I18nextProvider i18n={appServicesi18n}>
+      <ConfigProvider configUrl={`${__webpack_public_path__}config.json`}>
+        <AppWithKeycloak />
+      </ConfigProvider>
     </I18nextProvider>
   </Provider>
 );
 export default AppEntry;
-
