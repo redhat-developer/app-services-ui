d13609ae90832a9de9527b386a7842889cfb5497
Wojciech Trocki <wtrocki@redhat.com>
2021-06-02 16:53:11 +0100

    fix: integrate with SDK

--- hack/pullapi.sh
+++ hack/pullapi.sh
@@ -1,8 +1,6 @@
 
 echo "Synchronizing managed-services-api & AMS"
 
-curl --insecure -L -o hack/kas-fleet-manager.yaml https://gitlab.cee.redhat.com/service/kas-fleet-manager/-/raw/master/openapi/kas-fleet-manager.yaml
-
 curl  -L -o hack/ams.json https://api.stage.openshift.com/api/accounts_mgmt/v1/openapi
 
 echo "Finished synchronization with managed-services-api & AMS"
