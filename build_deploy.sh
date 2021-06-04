b43b46db53f360925d99a7b55a38c4c18bf40c38
Davide Bizzarri <dbizzarr@redhat.com>
2021-06-04 12:48:22 +0200

    push application-services-ui image to quay

--- build_deploy.sh
+++ build_deploy.sh
@@ -22,7 +22,7 @@ fi
 
 CONTAINER_ENGINE=${CONTAINER_ENGINE:-"docker"}
 VERSION="$(git log --pretty=format:'%h' -n 1)"
-IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-"mk-ui-host"}
+IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-"quay.io/rhoas/application-services-ui"}
 IMAGE_TAG=${IMAGE_TAG:-${VERSION}}
 IMAGE="${IMAGE_REPOSITORY}:${IMAGE_TAG}"
 
@@ -31,6 +31,18 @@ ${CONTAINER_ENGINE} build \
     -t ${IMAGE} \
     --build-arg NGINX_REGISTRY=quay.io/app-sre \
     -f ./build/Dockerfile .
+
+if [[ ! -z "${RHOAS_QUAY_USER}" ]] && [[ ! -z "${RHOAS_QUAY_TOKEN}" ]]; then
+    step "Push ui image"
+    ${CONTAINER_ENGINE} login --username "${RHOAS_QUAY_USER}" --password "${RHOAS_QUAY_TOKEN}"
+    
+    # update the latest image too
+    ${CONTAINER_ENGINE} tag ${IMAGE} ${IMAGE_REPOSITORY}:latest
+    
+    # push both tags
+    ${CONTAINER_ENGINE} push ${IMAGE}
+    ${CONTAINER_ENGINE} push ${IMAGE_REPOSITORY}:latest
+fi
 
 step "Push the client files"
 CID=$(${CONTAINER_ENGINE} create ${IMAGE})
