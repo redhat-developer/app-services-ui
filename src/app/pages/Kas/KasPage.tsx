354d0034fd5d3ee9eda9e4144ed8ac0577550348
Martin Marosi <marvusm.mmi@gmail.com>
2021-07-22 14:10:48 +0200

    Replace `react-router` imports with `react-router-dom`

--- src/app/pages/Kas/KasPage.tsx
+++ src/app/pages/Kas/KasPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory, useLocation } from 'react-router';
+import { useHistory, useLocation } from 'react-router-dom';
 import { useAuth, useConfig } from '@bf2/ui-shared';
 import { Configuration, DefaultApi, TermsReviewResponse } from '@openapi/ams';
 import { getTermsAppURL } from '@app/utils/termsApp';
