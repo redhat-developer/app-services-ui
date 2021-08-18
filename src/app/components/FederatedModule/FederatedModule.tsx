2480d5be315ad9597d79469b90a16c208ba3f22b
Pete Muir <pmuir@bleepbleep.org.uk>
2021-08-18 09:34:02 +0100

    fix: improve federated module state management
    Previously we saw a the dynamic script get reloaded if we had nested federated modules. By moving the dynamic script loading into a dedicated component and not loading that component until the module info is ready we avoid this problem.
    
    Also refactor the code into a number of files and add missing declarations / types
    Also cache the terms review state in a ref

--- src/app/components/FederatedModule/FederatedModule.tsx
+++ src/app/components/FederatedModule/FederatedModule.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-undef */
-import React, { ReactNode, useEffect, useState } from 'react';
-import { FederatedModuleConfig, useConfig, AssetsContext } from "@bf2/ui-shared";
+import React, { ComponentType, ReactNode, useEffect, useRef, useState } from 'react';
+import { AssetsContext } from "@bf2/ui-shared";
 import { Loading } from "@app/components/Loading/Loading";
-import { useRef } from 'react';
+import { getModuleInfo, ModuleInfo } from "@app/components/FederatedModule/moduleInfo";
+import { FederatedModuleContext } from "@app/components";
 
 const useIsMounted = () => {
   const isMounted = useRef(false)
@@ -16,49 +17,39 @@ const useIsMounted = () => {
   return isMounted
 }
 
-export type FederatedModuleContextProps = {
-  [module: string]: FederatedModuleConfig
-}
+declare function __webpack_init_sharing__(shareScope: string);
 
-const FederatedModuleContext = React.createContext<FederatedModuleContextProps>({});
+declare const __webpack_share_scopes__: {
+  default: unknown
+};
 
-export const FederatedModuleProvider: React.FunctionComponent = ({ children }) => {
-
-  const config = useConfig();
-
-  if (config === undefined) {
-    return <Loading/>;
-  }
-
-  return (
-    <FederatedModuleContext.Provider value={config.federatedModules}>
-      {children}
-    </FederatedModuleContext.Provider>
-  );
-}
+type Container = {
+  init: (shareScopes: unknown) => Promise<void>;
+  get: (module: string) => Promise<{ (): { default: ComponentType<any>; } }>;
+};
 
 function loadComponent(scope, module) {
   return async () => {
     // Initializes the share scope. This fills it with known provided modules from this build and all remotes
     await __webpack_init_sharing__('default');
-    const container = window[scope]; // or get the container somewhere else
+    const container = window[scope] as unknown as Container; // or get the container somewhere else
     // Initialize the container, it may provide shared modules
     await container.init(__webpack_share_scopes__.default);
-    const factory = await window[scope].get(module);
+    const factory = await container.get(module);
     const Module = factory();
     console.debug(`loaded ${module} from ${scope}`);
     return Module;
   };
 }
 
-const useDynamicScript = ({ url }) => {
+const useDynamicScript = (url: string) => {
   const isMounted = useIsMounted()
   const [ready, setReady] = React.useState(false);
   const [failed, setFailed] = React.useState(false);
 
   React.useEffect(() => {
     let element;
-    if(isMounted.current) {
+    if (isMounted.current) {
       if (!url) {
         setFailed(true);
         return;
@@ -88,7 +79,7 @@ const useDynamicScript = ({ url }) => {
     }
 
     return () => {
-      if(element) {
+      if (element) {
         console.log(`Dynamic federated module Removed: ${url}`);
         document.head.removeChild(element);
       }
@@ -105,7 +96,7 @@ export type FederatedModuleProps = {
   scope: string;
   module: string;
   render: (component: React.LazyExoticComponent<React.ComponentType<any>>) => ReactNode;
-  fallback?: any;
+  fallback?: React.ReactNode;
 }
 
 export const FederatedModule: React.FunctionComponent<FederatedModuleProps> = ({ scope, module, render, fallback }) => {
@@ -117,90 +108,55 @@ export const FederatedModule: React.FunctionComponent<FederatedModuleProps> = ({
   useEffect(() => {
     const fetchModuleInfo = async () => {
       const moduleInfo = await getModuleInfo(federatedModuleContext[scope].basePath, scope, federatedModuleContext[scope].fallbackBasePath);
-      if(isMounted.current) {
+      if (isMounted.current) {
         setModuleInfo(moduleInfo);
       }
     }
     fetchModuleInfo();
   }, [scope, federatedModuleContext]);
 
-  const { ready, failed } = useDynamicScript({ url: moduleInfo?.entryPoint });
+  if (moduleInfo !== undefined) {
+    return <DynamicFederatedModule scope={scope} module={module} render={render} moduleInfo={moduleInfo}/>
+  }
+  if (fallback !== undefined) {
+    return <>{fallback}</>;
+  }
+  return null;
+}
 
-  if (!ready || failed || !moduleInfo) {
-    if (fallback) {
-      return fallback;
+type DynamicFederatedModuleProps = FederatedModuleProps & {
+  moduleInfo: ModuleInfo
+}
+
+const DynamicFederatedModule: React.FunctionComponent<DynamicFederatedModuleProps> = ({
+                                                                                        moduleInfo,
+                                                                                        fallback,
+                                                                                        scope,
+                                                                                        render,
+                                                                                        module
+                                                                                      }) => {
+  const { ready, failed } = useDynamicScript(moduleInfo.entryPoint);
+
+  if (ready && !failed) {
+    const Component = React.lazy(
+      loadComponent(scope, module)
+    );
+
+    const getPath = () => {
+      return moduleInfo.basePath
     }
-    return null;
-  }
 
-  const Component = React.lazy(
-    loadComponent(scope, module)
-  );
-  const getPath = () => {
-    return moduleInfo.basePath
-  }
-
-  return (
-    <AssetsContext.Provider value={{ getPath }}>
-      <React.Suspense fallback={<Loading />}>
+    return (
+      <AssetsContext.Provider value={{ getPath }}>
+        <React.Suspense fallback={<Loading/>}>
           {render(Component)}
-      </React.Suspense>
-    </AssetsContext.Provider>
-  );
-}
-
-type ModuleInfo = {
-  entryPoint: string
-  basePath: string
-}
-
-const getModuleInfo = async (baseUrl: string, scope: string, fallbackBasePath?: string): Promise<ModuleInfo | undefined> => {
-
-  const fedModsJsonFileName = "fed-mods.json";
-
-  type FedMods = {
-    [key: string]: {
-      entry: string[],
-      modules: string[]
-    };
-  };
-
-  const fetchModuleInfo = async (basePath: string) => {
-    const url = `${basePath}/${fedModsJsonFileName}`;
-    const response = await fetch(url);
-    return await response.json()
-      .then(json => json as FedMods)
-      .then(fedMods => fedMods[scope])
-      .then(s => s.entry[0])
-      .then(path => {
-        if (path.startsWith(basePath)) {
-          return {
-            entryPoint: path,
-            basePath
-          };
-        }
-        return {
-          entryPoint: `${basePath}${path}`,
-          basePath
-        }
-      });
+        </React.Suspense>
+      </AssetsContext.Provider>
+    );
   }
 
-  try {
-    // First try to fetch the main entry point
-    return await fetchModuleInfo(baseUrl);
-  } catch (e) {
-    if (fallbackBasePath) {
-      try {
-        // If fetching the main entry point failed, and there is a fallback, try fetching that
-        // This allows us to use remote versions locally, transparently
-        return await fetchModuleInfo(fallbackBasePath)
-      } catch (e1) {
-        return undefined;
-      }
-    }
+  if (fallback) {
+    return <>{fallback}</>;
   }
-  return undefined;
+  return null;
 }
-
-
