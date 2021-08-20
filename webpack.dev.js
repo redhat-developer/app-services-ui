2124583df205dbca38e8904557ae2b46b6345737
Pete Muir <pmuir@bleepbleep.org.uk>
2021-08-20 12:55:41 +0100

    Fix: update deps

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,3 @@
-
-const path = require('path');
-const webpack = require('webpack');
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CopyPlugin = require('copy-webpack-plugin');
