11a469ab3692b486ab9d16d1f19f370e49b0ad8e
Joachim Schuler <jdh.schuler@gmail.com>
2021-09-16 06:10:26 -0400

    prevent infinite quick starts loading

--- src/app/pages/Resources/QuickStartLoaderFederated.tsx
+++ src/app/pages/Resources/QuickStartLoaderFederated.tsx
@@ -1,5 +1,5 @@
 import { FederatedModule } from '@app/components/FederatedModule/FederatedModule';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { useConfig } from '@bf2/ui-shared';
 import { Loading } from '@app/components/Loading/Loading';
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome/useChrome';
@@ -7,31 +7,31 @@ import useChrome from '@redhat-cloud-services/frontend-components/useChrome/useC
 export const appIdentifier = 'applicationServices';
 
 export const QuickStartLoaderFederated: FunctionComponent = () => {
+  const [loaded, setLoaded] = useState(false);
   const chrome = useChrome();
   const { quickStarts } = chrome;
 
   const config = useConfig();
   if (config === undefined) {
-    return <Loading/>;
+    return <Loading />;
   }
 
   const onLoad = (qs) => {
     if (quickStarts) {
+      setLoaded(true); // unload federated module
       quickStarts.set(appIdentifier, qs);
     }
-  }
+  };
 
-  return (<FederatedModule
+  return !loaded ? (
+    <FederatedModule
       scope="guides"
       module="./QuickStartLoader"
       render={(QuickStartLoaderFederated) => (
-        <QuickStartLoaderFederated
-          showDrafts={config?.guides.showDrafts}
-          onLoad={onLoad}
-        />
+        <QuickStartLoaderFederated showDrafts={config?.guides.showDrafts} onLoad={onLoad} />
       )}
     />
-  );
+  ) : null;
 };
 
 export default QuickStartLoaderFederated;
