ce95e4e5821360155e0222f3d189cf51bcd9b652
Riccardo Forina <rforina@redhat.com>
2021-12-17 08:58:15 +0100

    feat: handle non existant Kafka instances gracefully

--- src/app/pages/Kafka/kafka-instance.ts
+++ src/app/pages/Kafka/kafka-instance.ts
@@ -1,26 +1,25 @@
-import { useEffect, useState } from "react";
-import { Configuration, DefaultApi, KafkaRequest } from "@rhoas/kafka-management-sdk";
-import { useParams } from "react-router-dom";
-import { useAuth, useConfig } from "@rhoas/app-services-ui-shared";
+import { useEffect, useState } from 'react';
+import { Configuration, DefaultApi, KafkaRequest } from '@rhoas/kafka-management-sdk';
+import { useParams } from 'react-router-dom';
+import { useAuth, useConfig } from '@rhoas/app-services-ui-shared';
 
 export const getAdminServerUrl = (kafkaRequest?: KafkaRequest): string => {
   if (kafkaRequest === undefined) {
     throw new Error('kafkaRequest cannot be undefined');
   }
   return `https://admin-server-${kafkaRequest?.bootstrap_server_host}/rest`;
-}
+};
 
 export type KafkaInstance = {
-  kafkaDetail?: KafkaRequest
-  adminServerUrl?: string
-}
+  kafkaDetail: Required<KafkaRequest>;
+  adminServerUrl: string;
+};
 
-export const useKafkaInstance = (): KafkaInstance | undefined => {
+export const useKafkaInstance = (): KafkaInstance | false | undefined => {
   const config = useConfig();
   const auth = useAuth();
-  const [kafkaRequest, setKafkaRequest] = useState<KafkaRequest | undefined>();
+  const [kafkaRequest, setKafkaRequest] = useState<KafkaRequest | false | undefined>();
   const { id } = useParams<{ id: string }>();
-
   useEffect(() => {
     const getAdminApiUrl = async () => {
       if (auth === undefined || config === undefined) {
@@ -31,16 +30,21 @@ export const useKafkaInstance = (): KafkaInstance | undefined => {
         accessToken,
         basePath: config.kas.apiBasePath || '',
       } as Configuration);
-
-      const kafka = await kasService.getKafkaById(id);
-      setKafkaRequest(kafka.data);
+      try {
+        const kafka = await kasService.getKafkaById(id);
+        setKafkaRequest(kafka.data);
+      } catch (e) {
+        setKafkaRequest(false);
+      }
     };
 
     getAdminApiUrl();
   }, [auth, config, id]);
 
-  return kafkaRequest === undefined ? undefined : {
-    kafkaDetail: kafkaRequest,
-    adminServerUrl: getAdminServerUrl(kafkaRequest),
-  }
-}
+  return kafkaRequest
+    ? {
+        kafkaDetail: kafkaRequest as Required<KafkaRequest>,
+        adminServerUrl: getAdminServerUrl(kafkaRequest),
+      }
+    : kafkaRequest;
+};
