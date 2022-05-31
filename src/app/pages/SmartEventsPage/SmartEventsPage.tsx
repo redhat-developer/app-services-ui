db7d04744793320011f1b64e82af3706712892f6
roberto emanuel <kelvah@gmail.com>
2022-05-31 16:18:55 +0200

    pass config params to SmartEventsApp via props, update apiBasePath

--- src/app/pages/SmartEventsPage/SmartEventsPage.tsx
+++ src/app/pages/SmartEventsPage/SmartEventsPage.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import { FederatedModule } from '@app/components';
-import { AppServicesLoading } from '@rhoas/app-services-ui-components';
+import React from "react";
+import { FederatedModule } from "@app/components";
+import { AppServicesLoading } from "@rhoas/app-services-ui-components";
+import { useAuth, useBasename, useConfig } from "@rhoas/app-services-ui-shared";
 
 const SmartEventsPage: React.FC = () => {
+  const auth = useAuth();
+  const config = useConfig();
+  const basename = useBasename();
+
   return (
     <>
       <FederatedModule
         scope="smart_events"
         fallback={<AppServicesLoading />}
         module="./SmartEventsApp"
-        render={(SmartEventsApp) => <SmartEventsApp />}
+        render={(SmartEventsApp) => (
+          <SmartEventsApp
+            getToken={auth.smart_events.getToken}
+            getUsername={auth.getUsername}
+            apiBaseUrl={config.smart_events.apiBasePath}
+            basename={basename.getBasename()}
+          />
+        )}
       />
     </>
   );
