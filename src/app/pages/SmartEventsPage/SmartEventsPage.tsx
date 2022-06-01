7f99e3a7b12fe5950d840e72ede76b794129aa64
roberto emanuel <kelvah@gmail.com>
2022-06-01 11:30:08 +0200

    add SmartEventsPageConnected component

--- src/app/pages/SmartEventsPage/SmartEventsPage.tsx
+++ src/app/pages/SmartEventsPage/SmartEventsPage.tsx
@@ -1,30 +1,56 @@
-import React from "react";
+import React, {
+  LazyExoticComponent,
+  useMemo,
+  VoidFunctionComponent,
+} from "react";
 import { FederatedModule } from "@app/components";
 import { AppServicesLoading } from "@rhoas/app-services-ui-components";
 import { useAuth, useBasename, useConfig } from "@rhoas/app-services-ui-shared";
 
 const SmartEventsPage: React.FC = () => {
-  const auth = useAuth();
-  const config = useConfig();
-  const basename = useBasename();
-
   return (
-    <>
-      <FederatedModule
-        scope="smart_events"
-        fallback={<AppServicesLoading />}
-        module="./SmartEventsApp"
-        render={(SmartEventsApp) => (
-          <SmartEventsApp
-            getToken={auth.smart_events.getToken}
-            getUsername={auth.getUsername}
-            apiBaseUrl={config.smart_events.apiBasePath}
-            basename={basename.getBasename()}
-          />
-        )}
-      />
-    </>
+    <FederatedModule
+      scope="smart_events"
+      fallback={<AppServicesLoading />}
+      module="./SmartEventsApp"
+      render={(SmartEventsApp) => (
+        <SmartEventsPageConnected Component={SmartEventsApp} />
+      )}
+    />
   );
 };
 
 export default SmartEventsPage;
+
+const SmartEventsPageConnected: VoidFunctionComponent<{
+  Component: LazyExoticComponent<any>;
+}> = ({ Component }) => {
+  const {
+    smart_events: { getToken },
+    getUsername,
+  } = useAuth();
+  const config = useConfig();
+  const { getBasename } = useBasename();
+
+  const basename = getBasename();
+  const apiBaseUrl = config.smart_events.apiBasePath;
+
+  const props = useMemo<SmartEventsAppProps>(
+    () => ({
+      getToken,
+      getUsername,
+      apiBaseUrl,
+      basename,
+    }),
+    [getToken, getUsername, apiBaseUrl, basename]
+  );
+
+  return <Component {...props} />;
+};
+
+type SmartEventsAppProps = {
+  getToken: () => Promise<string> | undefined;
+  getUsername: () => Promise<string> | undefined;
+  apiBaseUrl: string;
+  basename: string;
+};
