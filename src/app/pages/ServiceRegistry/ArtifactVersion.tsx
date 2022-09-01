e1fa3e209fcc824c7ad2327a59ee749e5111c129
Eric Wittmann <eric.wittmann@gmail.com>
2022-09-01 14:18:46 -0400

    Pass the principals to the artifact version page

--- src/app/pages/ServiceRegistry/ArtifactVersion.tsx
+++ src/app/pages/ServiceRegistry/ArtifactVersion.tsx
@@ -1,9 +1,11 @@
-import { FunctionComponent } from "react";
+import { useState, useEffect, FunctionComponent } from "react";
 import { useParams } from "react-router-dom";
 import { FederatedApicurioComponent } from "@app/pages/ServiceRegistry/FederatedApicurioComponent";
 import { SrsLayout } from "@app/pages/ServiceRegistry/SrsLayout";
-import { useConfig } from "@rhoas/app-services-ui-shared";
+import { useConfig, useAuth } from "@rhoas/app-services-ui-shared";
 import { ServiceDownPage } from "@app/pages";
+import { usePrincipal } from "@app/components";
+import { AppServicesLoading } from "@rhoas/app-services-ui-components";
 
 export const ArtifactVersionDetails: FunctionComponent = () => {
   const config = useConfig();
@@ -19,16 +21,37 @@ const ArtifactVersionDetailsConnected: FunctionComponent = () => {
   let { artifactId } = useParams<{ artifactId: string }>();
   artifactId = decodeURIComponent(artifactId);
 
+  const [currentlyLoggedInuser, setCurrentlyLoggedInuser] = useState<string>();
+  const auth = useAuth();
+  const { getAllPrincipals } = usePrincipal();
+
+  useEffect(() => {
+    (async () => {
+      await auth?.getUsername()?.then((user) => setCurrentlyLoggedInuser(user));
+    })();
+  }, [auth]);
+
   return (
     <SrsLayout
       breadcrumbId="srs.artifacts_details"
       artifactId={artifactId}
-      render={(registry) => (
-        <FederatedApicurioComponent
-          registry={registry}
-          module="./FederatedArtifactVersionPage"
-        />
-      )}
+      render={(registry) => {
+        if (registry === undefined || currentlyLoggedInuser === undefined) {
+          return <AppServicesLoading />;
+        }
+
+        const principals = getAllPrincipals()?.filter(
+          (p) => p.id !== currentlyLoggedInuser && p.id !== registry?.owner
+        );
+
+        return (
+          <FederatedApicurioComponent
+            registry={registry}
+            module="./FederatedArtifactVersionPage"
+            principals={principals}
+          />
+        );
+      }}
     />
   );
 };
