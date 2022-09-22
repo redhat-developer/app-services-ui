01458fe66b6b2559a082329c4171eae2363c6b4b
hemahg <hhg@redhat.com>
2022-09-22 19:41:14 +0530

    Integrate api card
    Signed-off-by: hemahg <hhg@redhat.com>

--- src/app/pages/Overview/OverviewPage.tsx
+++ src/app/pages/Overview/OverviewPage.tsx
@@ -1,4 +1,4 @@
-import { OverviewPageV2 } from "@rhoas/app-services-ui-components";
+import { OverviewPageV4 } from "@rhoas/app-services-ui-components";
 import { FunctionComponent } from "react";
 
 export const ConnectedOverviewPage: FunctionComponent = () => {
@@ -7,11 +7,14 @@ export const ConnectedOverviewPage: FunctionComponent = () => {
 
   const connectorHref = "/connectors";
 
+  const APIDesignHref = "/api-designer";
+
   return (
-    <OverviewPageV2
+    <OverviewPageV4
       toKafkaHref={kafkaHref}
       toServiceRegistryHref={serviceRegistryHref}
       toConnectorsHref={connectorHref}
+      toAPIDesignHref={APIDesignHref}
     />
   );
 };
