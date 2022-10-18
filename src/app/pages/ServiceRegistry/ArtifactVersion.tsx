deeb26d5ac1d0a3b86552246ac4ab4c9c9e97c3e
Eric Wittmann <eric.wittmann@gmail.com>
2022-10-18 10:34:19 -0400

    Move principal logic to ServiceAccountsPageConnected

--- src/app/pages/ServiceRegistry/ArtifactVersion.tsx
+++ src/app/pages/ServiceRegistry/ArtifactVersion.tsx
@@ -2,9 +2,8 @@ import { useState, useEffect, FunctionComponent } from "react";
 import { useParams } from "react-router-dom";
 import { FederatedApicurioComponent } from "@app/pages/ServiceRegistry/FederatedApicurioComponent";
 import { SrsLayout } from "@app/pages/ServiceRegistry/SrsLayout";
-import { useConfig, useAuth, Principal } from "@rhoas/app-services-ui-shared";
+import { useConfig, useAuth } from "@rhoas/app-services-ui-shared";
 import { ServiceDownPage } from "@app/pages";
-import { usePrincipal } from "@app/components";
 import { AppServicesLoading } from "@rhoas/app-services-ui-components";
 import { Registry } from "@rhoas/registry-management-sdk";
 
@@ -45,24 +44,14 @@ const ArtifactVersionDetailsLayoutRender: FunctionComponent<{
     })();
   }, [auth]);
 
-  const { getAllPrincipals } = usePrincipal();
-
   if (registry === undefined || currentlyLoggedInuser === undefined) {
     return <AppServicesLoading />;
   }
-
-  const getPrincipals: () => Principal[] = () => {
-    const principals = getAllPrincipals()?.filter(
-      (p) => p.id !== currentlyLoggedInuser && p.id !== registry?.owner
-    );
-    return principals;
-  };
 
   return (
     <FederatedApicurioComponent
       registry={registry}
       module="./FederatedArtifactVersionPage"
-      principals={getPrincipals}
     />
   );
 };
