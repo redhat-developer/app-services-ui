f19896a5911da7cd98356e37a6740042e306d55b
hemahg <hhg@redhat.com>
2023-05-03 20:42:56 +0530

    Remove unused props
    Signed-off-by: hemahg <hhg@redhat.com>

--- src/app/pages/Overview/OverviewPage.tsx
+++ src/app/pages/Overview/OverviewPage.tsx
@@ -2,21 +2,7 @@ import { OverviewPageV4 } from "@rhoas/app-services-ui-components";
 import { FunctionComponent } from "react";
 
 export const ConnectedOverviewPage: FunctionComponent = () => {
-  const kafkaHref = "/streams/kafkas";
-  const serviceRegistryHref = "/service-registry";
-
-  const connectorHref = "/connectors";
-
-  const APIDesignHref = "/api-designer";
-
-  return (
-    <OverviewPageV4
-      toKafkaHref={kafkaHref}
-      toServiceRegistryHref={serviceRegistryHref}
-      toConnectorsHref={connectorHref}
-      toAPIDesignHref={APIDesignHref}
-    />
-  );
+  return <OverviewPageV4 />;
 };
 
 export default ConnectedOverviewPage;
